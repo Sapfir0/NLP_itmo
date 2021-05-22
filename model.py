b218c112a039f87d89bd4bbd0d069250970f72a1
Sapfir0 <sapfir999999@yandex.ru>
2021-05-22 09:36:21 +0300

    0.79

--- model.py
+++ model.py
@@ -1,92 +1,70 @@
 import numpy as np
 import pandas as pd
 import pymorphy2
-from catboost import CatBoostRegressor
-from sklearn.tree import ExtraTreeRegressor
-from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import ExtraTreesClassifier, RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression, Ridge
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import ExtraTreesRegressor, VotingRegressor, RandomForestRegressor
-from sklearn.neural_network import MLPRegressor
-from sklearn.preprocessing import PolynomialFeatures, StandardScaler
+from catboost import CatBoostClassifier, Pool
+from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import make_pipeline
-from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
 class Model:
     def __init__(self):
         self.morph = pymorphy2.MorphAnalyzer()
-        self.alphabet = 'abcdefghijklmnopqrstuvwxyzабвгдежзийклмнопрстуфхцчшщъыьэюя '
+        self.alphabet = 'abcdefghijklmnopqrstuvwxyzабвгдежзийклмнопрстуфхцчшщъыьэюя .'
 
     def normalize_text_with_morph(self, x):
         x = x.lower().replace("ё", "е")
         words = ''.join([[" ", i][i in self.alphabet] for i in x]).lower().split()
         return ' '.join([self.morph.parse(w)[0].normal_form for w in words])
 
-
-    def getModel(self, _train, trueData):
-        X_train, X_test, y_train, y_test = train_test_split(_train, trueData, test_size=0.2, random_state=42)
-        print(y_train)
-        models = [ 
-            {
-                'model': CatBoostRegressor(
-                    task_type='GPU',
-                    random_state=42, 
-                    allow_const_label=True
-                ),
-                'fitParams': {'eval_set': (X_test, y_test), 'use_best_model': True}
-            },
-            {
-                'model': ExtraTreesRegressor(n_jobs=-1),
-                'fitParams': {}
-            }
-        ]
-
-        scores = []
-
-        for modelInfo in models:
-            model = modelInfo['model']
-            
-            # model.fit(_train, trueData, **modelInfo['fitParams'])
-            model.fit(X_train, y_train, **modelInfo['fitParams'])
-
-            predictedData = model.predict(X_test)
-            score = accuracy_score(y_test, np.round(np.clip(predictedData, 0, 1)))
-            scores.append(score)
-
-        bestIndex = scores.index(max(scores))
-        print(scores)
-
-        return models[bestIndex]['model']
-
-
-
-    def _fit_predict(self, train, test):
-        vectorizer_a = TfidfVectorizer(max_features=1000)
-        vectorizer_b = TfidfVectorizer(max_features=1000)
-
+    def prepare_test_train(self, train, test):
         train["message_a"] = train["message_a"].apply(self.normalize_text_with_morph)
         train["message_b"] = train["message_b"].apply(self.normalize_text_with_morph)
-        train_a = vectorizer_a.fit_transform(train["message_a"]).toarray()
-        train_b = vectorizer_b.fit_transform(train["message_b"]).toarray()
-        _train = np.hstack([train_a, train_b])
-
         test["message_a"] = test["message_a"].apply(self.normalize_text_with_morph)
         test["message_b"] = test["message_b"].apply(self.normalize_text_with_morph)
-        test_a = vectorizer_a.transform(test["message_a"]).toarray()
-        test_b = vectorizer_b.transform(test["message_b"]).toarray()
-        _test = np.hstack([test_a, test_b])
 
+        return train[['message_a', 'message_b']], test[['message_a', 'message_b']]
 
-        model = self.getModel(_train, train["target"])
-        print(model)
-        model.fit(_train, train["target"])
-        predicted = model.predict(_test)
-        
+    def _fit_predict(self, train, test):
+        X, test_fixed = self.prepare_test_train(train, test)
+        y = train['target']
+        params = {
+            'dictionaries': [
+                {
+                    'dictionary_id': 'Word',
+                    'max_dictionary_size': '50000',
+                    "occurrence_lower_bound" : "2",
+                }
+            ],
+            'feature_calcers': [
+                'BoW:top_tokens_count=10000'
+            ]
+        }
+        model = CatBoostClassifier(
+            verbose=100, 
+            loss_function='CrossEntropy', 
+            eval_metric='Accuracy',
+            task_type='GPU',
+            **params
+        )
+        learn_pool = Pool(
+            X, y,
+            text_features=['message_a', 'message_b'],
+            feature_names=['message_a', 'message_b'],
+        )
+        test_pool = Pool(
+            test_fixed,
+            text_features=['message_a', 'message_b'],
+            feature_names=['message_a', 'message_b'],
+        )
 
-        return pd.DataFrame(np.round(np.clip(predicted, 0, 1)), columns=["target"])
+        model.fit(learn_pool)
+        predict = model.predict(test_pool)
+        return pd.DataFrame(np.round(np.clip(predict, 0, 1)), columns=["target"])
 
     def fit_predict(self,
                     train_1, test_1,
@@ -100,4 +78,3 @@ class Model:
         predicted_4 = self._fit_predict(train_4, test_4)
         predicted_5 = self._fit_predict(train_5, test_5)
         return [predicted_1, predicted_2, predicted_3, predicted_4, predicted_5]
-
